abd65a372b3033ab0c417a6b4656f5fe8f03bc99
Levi Flory <lsflory@yahoo.com>
2022-08-29 10:48:27 -0500

    Added starter project fo the HiringRequestQueueProcesser.

---
+++ HiringRequestQueueProcesser/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
